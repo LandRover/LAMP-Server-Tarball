cbb78387b73dc4075a3314f8a19581c93dc08772
Oleg <github.com@landroverz.com>
2014-11-18 11:12:39 +0200

    added helpers

---
+++ usr/src/.BUILD/helpers/ln.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+# @todo: move all internal LNs here..
---
+++ usr/src/.BUILD/helpers/prerequisites_debian.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get install \
+build-essential \
+pkg-config \
+make \
+autoconf \
+automake1.9 \
+gcc-4.1 \
+g++;
