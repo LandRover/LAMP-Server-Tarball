9cdf25b3bfc2599308d5bc0391b8e929d0ad8536
Oleg <github.com@landroverz.com>
2014-11-18 14:24:24 +0200

    added libgb 2.1

---
+++ usr/src/.BUILD/libgd.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+VERSION="2.1.0";
+APP_NAME="libgd";
+OPT="/opt/local/sbin";
+
+cd ../${APP_NAME};
+
+make clean;
+
+## http://www.linuxforums.org/forum/programming-scripting/47173-make-error-giving-gd-2-0-33-a.html
+## modify gd_png.c and replace 'png.h' to: '/opt/local/sbin/libpng/include/png.h'
+
+./configure \
+--prefix=${OPT}/${APP_NAME}-${VERSION} \
+--with-jpeg=/opt/local/sbin/jpeg \
+--with-png=/opt/local/sbin/libpng \
+--with-freetype=/opt/local/sbin/freetype \
+--with-fontconfig=/opt/local/sbin/fontconfig;
+
+make;
+make install;
+
+rm -rf ${OPT}/${APP_NAME};
+ln -s ${OPT}/${APP_NAME}-${VERSION} ${OPT}/${APP_NAME};
