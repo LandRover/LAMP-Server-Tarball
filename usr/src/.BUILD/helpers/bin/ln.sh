269fb23ef61c6771ec77af455f1fb828e667f033
Oleg <github.com@landroverz.com>
2014-11-19 11:02:14 +0200

    link creating wrapper added

--- usr/src/.BUILD/helpers/bin/ln.sh
+++ usr/src/.BUILD/helpers/bin/ln.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 
-# @todo: move all internal LNs here..
+#!/bin/bash
+
+SRC="$1";
+DST="$2";
+
+function usage {
+    echo "";
+    echo "USAGE: $0 <src> <dst>";
+    echo "";
+    echo "<src> - source dir for the link";
+    echo "<dst> - destination dir for link";
+    echo "";
+
+    exit 0;
+}
+
+[[ -z "${SRC}" || -z "${DST}" ]] && usage;
+
+if [ ! -d "${SRC}" ]; then
+    echo "[error] Soruce dir, ${SRC}, not found. Fix it and try again.";
+    usage;
+fi
+
+if [ -d "${DST}" ]; then
+    echo "[info] Existing symlink detected at ${DST}. Removing..";
+    rm -rf ${DST};
+    echo "[info] ${DST} Removed link.";
+fi
+
+echo "[info] Creating link ${DST}";
+ln -s ${SRC} ${DST};
+echo "[info] Link created ${DST} -> ${SRC}";
