77522e26e9a2091422f7de609e7aa78ff25899cd
Oleg <github.com@landroverz.com>
2014-11-19 09:30:17 +0200

    final build script for apache 2.4

--- usr/src/.BUILD/httpd.sh
+++ usr/src/.BUILD/httpd.sh
@@ -9,42 +9,45 @@ cd ../${APP_NAME};
 make clean;
 
 groupadd apache;
-useradd -d /var/www/ -g apache -s /bin/false apache;
+useradd -d /opt/local/sbin/httpd/htdocs -g apache -s /bin/false apache;
 
 ./configure \
 --prefix=${OPT}/${APP_NAME}-${VERSION} \
---enable-so \
---enable-ssl \
---enable-info \
---enable-cache \
---enable-file-cache \
---enable-disk-cache \
---enable-mem-cache \
+--disable-alias \
+--disable-setenvif \
+--enable-deflate \
+--enable-dir \
 --enable-expires \
 --enable-headers \
---enable-proxy \
+--enable-info \
 --enable-mime-magic \
---enable-vhost-alias \
---enable-usertrack \
---enable-deflate \
+--enable-mods-shared='authz_core authz_host authz_basic suexec headers proxy status info mime deflate rewrite dir slotmem_shm ssl unique_id' \
+--enable-mods-static='unixd' \
+--enable-modules='none' \
+--enable-mpms-shared='event worker' \
+--enable-proxy \
 --enable-rewrite \
+--enable-slotmem-shm \
+--enable-so \
+--enable-ssl \
+--enable-status \
+--enable-suexec \
 --enable-unique-id \
 --enable-unixd \
---enable-mods-static='unixd' \
---enable-suexec \
+--enable-vhost-alias \
+--sysconfdir=/opt/local/etc/apache \
+--with-apr-util=/opt/local/sbin/apr-util \
+--with-apr=/opt/local/sbin/apr \
+--with-expat=builtin \
+--with-pcre=/opt/local/sbin/pcre \
+--with-ssl=/opt/local/sbin/openssl \
 --with-suexec \
 --with-suexec-caller=apache \
---with-suexec-docroot=/var/www \
---with-suexec-logfile=/var/log/httpd/suexec_log \
---with-suexec-userdir=public_html \
---with-suexec-uidmin=100 \
+--with-suexec-docroot=/opt/local/sbin/httpd/htdocs \
 --with-suexec-gidmin=100 \
---with-ssl=/opt/local/sbin/openssl \
---with-apr=/opt/local/sbin/apr \
---with-apr-util=/opt/local/sbin/apr-util \
---with-pcre=/opt/local/sbin/pcre \
---with-expat=builtin \
---sysconfdir=/opt/local/etc/apache;
+--with-suexec-logfile=/var/log/httpd/suexec_log \
+--with-suexec-uidmin=100 \
+--with-suexec-userdir=public_html;
 
 make;
 make install;
@@ -56,3 +59,10 @@ ln -s ${OPT}/${APP_NAME}-${VERSION} ${OPT}/${APP_NAME};
 
 rm -rf /opt/local/etc/init.d/${APP_NAME}
 ln -s ${OPT}/${APP_NAME}/bin/apachectl /opt/local/etc/init.d/${APP_NAME};
+
+chown -R apache:apache ${OPT}/${APP_NAME}
+chmod -R go-rwx ${OPT}/${APP_NAME}
+chmod -R r-w ${OPT}/${APP_NAME}
+chmod o+x ${OPT}/${APP_NAME} ${OPT}/${APP_NAME}/htdocs ${OPT}/${APP_NAME}/cgi-bin
+chmod -R o+r ${OPT}/${APP_NAME}/htdocs
+chmod -R u+w ${OPT}/${APP_NAME}/logs
