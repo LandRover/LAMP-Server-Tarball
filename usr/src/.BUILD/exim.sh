4a77561011d555e488a6197e5148a63f696755fb
Oleg <github.com@landroverz.com>
2014-11-20 17:13:19 +0200

    added exim

---
+++ usr/src/.BUILD/exim.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# build data
+BUILD="../${PWD##*/}";
+VERSION="4.84";
+APP_NAME="exim";
+
+# destination build info
+LOCAL="/opt/local";
+BIN_DIR="${LOCAL}/sbin";
+ETC_DIR="${LOCAL}/etc";
+DESTINATION="${BIN_DIR}/${APP_NAME}-${VERSION}";
+
+cd ../${APP_NAME};
+
+make clean;
+
+./configure \
+--prefix=${DESTINATION};
+
+make;
+make install;
+
+[ -a "${BUILD}/post_build/$0" ] && cd ${BUILD}/post_build; $0 ${BIN_DIR} ${APP_NAME} ${VERSION};
