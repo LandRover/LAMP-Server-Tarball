522ea36f7bceae2046720524d636de7328aa2110
Oleg <github.com@landroverz.com>
2014-11-21 08:15:43 +0200

    added post build script for exim

--- usr/src/.BUILD/post_build/exim.sh
+++ usr/src/.BUILD/post_build/exim.sh
@@ -3,4 +3,11 @@
 source ../helpers/.post_validate_input.sh;
 source ./.shared.sh;
 
-../helpers/post_etc_ln.sh "${APP_NAME}" "init.d" "${APP_NAME}";
+## init.d
+../helpers/post_etc_ln.sh "${ETC_DIR}" "init.d" "${APP_NAME}";
+
+## logrotate.d
+../helpers/post_etc_ln.sh "${ETC_DIR}" "logrotate.d" "${APP_NAME}";
+
+## system stop/start on boot
+update-rc.d ${APP_NAME} defaults
