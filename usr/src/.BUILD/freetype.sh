6ab82dcca4fc8a300738e6d2d56d75e3edecf6ef
Oleg <github.com@landroverz.com>
2014-11-21 15:23:40 +0200

    removed libpng from freetype

--- usr/src/.BUILD/freetype.sh
+++ usr/src/.BUILD/freetype.sh
@@ -13,12 +13,9 @@ DESTINATION="${BIN_DIR}/${APP_NAME}-${VERSION}";
 
 source ./helpers/.pre_build_unpack.sh; ##unpack tar and enters the app dir
 
-export LIBPNG_CFLAGS="-I${BIN_DIR}/libpng/include";
-export LIBPNG_LDFLAGS="-L${BIN_DIR}/libpng/lib";
-
 ./configure \
---with-png=yes \
---prefix=${DESTINATION};
+--prefix=${DESTINATION} \
+--with-png=no;
 
 make;
 make install;
