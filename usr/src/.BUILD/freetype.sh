dbcf5ac90662a5e744f915ca307b6a62d215e085
Oleg <github.com@landroverz.com>
2014-11-21 15:03:17 +0200

    added freetype libpng support

--- usr/src/.BUILD/freetype.sh
+++ usr/src/.BUILD/freetype.sh
@@ -13,7 +13,11 @@ DESTINATION="${BIN_DIR}/${APP_NAME}-${VERSION}";
 
 source ./helpers/.pre_build_unpack.sh; ##unpack tar and enters the app dir
 
+export LIBPNG_CFLAGS="-I${BIN_DIR}/libpng/include";
+export LIBPNG_LDFLAGS="-L${BIN_DIR}/libpng/lib";
+
 ./configure \
+--with-png=yes \
 --prefix=${DESTINATION};
 
 make;
