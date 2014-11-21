154c09f0828ed117b5f367306cfe38bd71d48873
Oleg <github.com@landroverz.com>
2014-11-21 08:28:05 +0200

    added logs dir creation

--- usr/src/.BUILD/post_build/exim.sh
+++ usr/src/.BUILD/post_build/exim.sh
@@ -4,6 +4,8 @@ source ../helpers/.post_validate_input.sh;
 source ./.shared.sh;
 
 ETC_DIR="/opt/local/etc";
+USER="$4";
+[ -z "${USER}" ] && usage "[error] User was not set. Halt. As apache setup requires a user.";
 
 ## init.d
 ../helpers/post_etc_ln.sh "${ETC_DIR}" "init.d" "${APP_NAME}";
@@ -13,3 +15,7 @@ ETC_DIR="/opt/local/etc";
 
 ## system stop/start on boot
 update-rc.d ${APP_NAME} defaults
+
+## create logs dir
+mkdir /var/log/${APP_NAME};
+chown ${USER}:${USER} /var/log/${APP_NAME};
