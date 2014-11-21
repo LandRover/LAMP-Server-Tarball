90c84004a101215cc253c66d17d4dc3f8629aa32
Oleg <github.com@landroverz.com>
2014-11-21 15:05:11 +0200

    removed final /

--- usr/src/.BUILD/mcrypt.sh
+++ usr/src/.BUILD/mcrypt.sh
@@ -14,8 +14,8 @@ DESTINATION="${BIN_DIR}/${APP_NAME}-${VERSION}";
 source ./helpers/.pre_build_unpack.sh; ##unpack tar and enters the app dir
 
 export LD_LIBRARY_PATH=${BIN_DIR}/libmcrypt/lib:${BIN_DIR}/mhash/lib;
-export LDFLAGS="-L${BIN_DIR}/mhash/lib/ -I${BIN_DIR}/mhash/include/";
-export CFLAGS="-I${BIN_DIR}/mhash/include/";
+export LDFLAGS="-L${BIN_DIR}/mhash/lib -I${BIN_DIR}/mhash/include";
+export CFLAGS="-I${BIN_DIR}/mhash/include";
 
 ./configure \
 --prefix=${DESTINATION} \
