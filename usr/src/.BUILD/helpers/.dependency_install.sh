cc6135d59c4b3104efb7b04af578d6d314986f75
Oleg <github.com@landroverz.com>
2014-11-23 08:38:13 +0200

    initial

---
+++ usr/src/.BUILD/helpers/.dependency_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+[ "$0" = "${BASH_SOURCE}" ] && echo "FILE CAN ONLY BE SOURCED, DIRECT EXECUTION IS NOT ALLOWED!" && exit 0;
+
+# Unpacks source packages at /usr/src
+# file is loaded via SOURCE and must NOT BE executed directly.
+
+function error {
+    echo $1;
+    echo "";
+
+    exit 0;
+}
+
