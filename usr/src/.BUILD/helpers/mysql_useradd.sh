410b844734deb78eb80cbc7a2c4dd3098ebbbd98
Oleg <github.com@landroverz.com>
2014-11-23 12:32:46 +0200

    created a script for generating a mysql user

---
+++ usr/src/.BUILD/helpers/mysql_useradd.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+DBNAME="$1";
+DBUSER="$2";
+E_BADARGS="65";
+RANDOM_PASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w8 | head -n1);
+DBPASS="${3:-$RANDOM_PASSWORD}";
+MSQL_BIN="/opt/local/sbin/mysql/bin/mysql";
+
+function usage {
+    if [ ! -z "$1" ]; then
+        echo $1;
+        echo "";
+    fi
+
+    echo "USAGE: $0 <db_name> <db_user> [db_pass]";
+    echo "";
+    echo "<db_name> - Database name.";
+    echo "<db_user> - Database username.";
+    echo "[db_pass] - If password not set, will generate a random password.";
+    echo "";
+
+    exit ${E_BADARGS};
+}
+
+## validation inputs
+[[ -z "${DBNAME}" || -z "${DBUSER}" || -z "${DBPASS}" ]] && usage;
+
+Q1="CREATE DATABASE IF NOT EXISTS ${DBNAME};";
+Q2="GRANT ALL ON *.* TO '${DBUSER}'@'localhost' IDENTIFIED BY '${DBPASS}';";
+Q3="FLUSH PRIVILEGES;";
+SQL="${Q1}${Q2}${Q3}";
+
+$MSQL_BIN -uroot -e "${SQL}";
+
+echo "[info] User created with the following:";
+echo "    DB IP: localhost";
+echo "    DB NAME: ${DBNAME}";
+echo "    DB USERNAME: ${DBUSER}";
+echo "    DB PASSWORD: ${DBPASS}";
+echo "";
