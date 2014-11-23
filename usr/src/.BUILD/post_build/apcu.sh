625a89d3f6b1a5dfa171633e9d0089a46beeb42b
Oleg <github.com@landroverz.com>
2014-11-23 07:31:43 +0200

    postbuild script for apcu module created

--- usr/src/.BUILD/post_build/apcu.sh
+++ usr/src/.BUILD/post_build/apcu.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-source ../helpers/.post_validate_input.sh;
+ETC_DIR="/opt/local/etc";
+ETC_PHPD="${ETC_DIR}/php/php.d";
 
-echo $PWD;
+echo "[info] Deploying apcu.ini default settings.";
+cp -Lf  ../templates/php/apcu/apcu.ini ${ETC_PHPD}/;
+
+echo "[info] Restarting PHP-FPM...";
+/etc/init.d/php-fpm restart;
