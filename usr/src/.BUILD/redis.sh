1043de6563e828e1acac7dab7713f6229e315e36
Oleg <github.com@landroverz.com>
2014-11-25 15:17:21 +0200

    added user

--- usr/src/.BUILD/redis.sh
+++ usr/src/.BUILD/redis.sh
@@ -3,6 +3,7 @@
 # build data
 BUILD="../${PWD##*/}";
 VERSION="2.8.17";
+USER="redis";
 APP_NAME="redis";
 
 # destination build info
@@ -13,6 +14,8 @@ DESTINATION="${BIN_DIR}/${APP_NAME}-${VERSION}";
 
 source ./helpers/.dependency_install.sh; ##checks all @DEPENDENCIES in tact
 source ./helpers/.pre_build_unpack.sh; ##unpack tar and enters the app dir
+
+[ -z "$(getent passwd ${USER})" ] && echo "[info] User ${USER} not found, creating.." && useradd -M -s /bin/false -d /home/${USER}/ ${USER};
 
 ./configure \
 --prefix=${DESTINATION};
