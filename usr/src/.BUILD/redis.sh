4e7093d7685d71cdb553afab6b539d435c7c582b
Oleg <github.com@landroverz.com>
2015-02-12 08:53:57 +0200

    version bump

--- usr/src/.BUILD/redis.sh
+++ usr/src/.BUILD/redis.sh
@@ -17,7 +17,7 @@ source ./helpers/.pre_build_unpack.sh; ##unpack tar and enters the app dir
 
 [ -z "$(getent passwd ${USER})" ] && echo "[info] User ${USER} not found, creating.." && useradd -M -s /bin/false -d /home/${USER}/ ${USER};
 
-make;
+make -j y;
 make PREFIX=${DESTINATION} install;
 
 [ -a "${BUILD}/post_build/$0" ] && cd ${BUILD}/post_build; $0 ${BIN_DIR} ${APP_NAME} ${VERSION};
