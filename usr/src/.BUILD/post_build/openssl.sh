3da2e296da74f6bb09f499092fe7c57e0e08c974
Oleg <github.com@landroverz.com>
2015-02-13 12:18:34 +0200

    added link to includes of openssl

--- usr/src/.BUILD/post_build/openssl.sh
+++ usr/src/.BUILD/post_build/openssl.sh
@@ -6,3 +6,5 @@ source ./.shared.sh;
 ## add ld.so to global
 echo "${BIN_DIR}/${APP_NAME}/lib" > "/etc/ld.so.conf.d/${APP_NAME}.conf";
 /sbin/ldconfig ${BIN_DIR}/${APP_NAME}/lib;
+
+ln -s ${BIN_DIR}/${APP_NAME}/include /usr/include/openssl;
