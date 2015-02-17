d64b76cb218819df66f0ea85cb4543272deda877
OG <github.com@landroverz.com>
2015-02-17 13:44:07 +0200

    Update apache_useradd.sh

--- usr/src/.BUILD/helpers/apache_useradd.sh
+++ usr/src/.BUILD/helpers/apache_useradd.sh
@@ -54,7 +54,7 @@ function usage {
 [[ -z "${USER}" || -z "${DOMAIN}" || $PRIORITY != ?(-)+([0-9.]) || $PORT != ?(-)+([0-9.]) || $IS_SSL != ?(-)+([0-1.]) ]] && usage;
 
 [ -z "$(getent passwd ${USER})" ] && echo "[info] User ${USER} not found, creating.." && useradd -M -s /bin/false -d ${HTDOCS} ${USER};
-[ ! -d "${HTDOCS}" ] && mkdir -p ${HTDOCS} && chmod 710 ${HTDOCS}/../ && echo "Hello World!" > ${HTDOCS} && chmod -R 600 ${HTDOCS} && chown -R ${USER}:${USER} ${HTDOCS}/.. ;
+[ ! -d "${HTDOCS}" ] && mkdir -p ${HTDOCS} && chmod 710 ${HTDOCS}/../ && echo "Hello World!" > ${HTDOCS} && chown -R ${USER}:${USER} ${HTDOCS}/.. && chmod -R 600 ${HTDOCS};
 [ ! -d "${APACHE_LOGS_DIR}" ] && mkdir -p ${APACHE_LOGS_DIR} && chown -R ${USER}:${USER} ${APACHE_LOGS_DIR} && chmod -R u+w ${APACHE_LOGS_DIR};
 
 echo "[info] Copy PHP-FPM Template ${TARGET_PHPFPM_FILE}";
