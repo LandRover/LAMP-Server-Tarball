c0de92837e7b5f5cb9c2734a3af92b2409f835f9
Oleg <github.com@landroverz.com>
2015-04-21 10:11:10 +0300

    scons build changed to copy release

--- usr/src/.BUILD/scons.sh
+++ usr/src/.BUILD/scons.sh
@@ -11,16 +11,10 @@ BIN_DIR="${LOCAL}/sbin";
 ETC_DIR="${LOCAL}/etc";
 DESTINATION="${BIN_DIR}/${APP_NAME}-${VERSION}";
 
-## wierd notation by scons.. with the src thing in name
-rm -rf ../${APP_NAME}-${VERSION}; ln -s /usr/src/${APP_NAME}-src-${VERSION}/ /usr/src/${APP_NAME}-${VERSION};
-
 source ./helpers/.dependency_install.sh; ##checks all @DEPENDENCIES in tact
 source ./helpers/.pre_build_unpack.sh; ##unpack tar and enters the app dir
 
-./configure \
---prefix=${DESTINATION};
-
-make;
-make install;
+rm -rf ${DESTINATION};
+cp -R ../${APP_NAME}-${VERSION} ${DESTINATION};
 
 [ -a "${BUILD}/post_build/$0" ] && cd ${BUILD}/post_build; $0 ${BIN_DIR} ${APP_NAME} ${VERSION};
