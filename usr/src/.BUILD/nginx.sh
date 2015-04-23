1c88d8f339bd2cf4af34aef889d656c6c77568c6
Oleg <github.com@landroverz.com>
2015-04-23 11:12:13 +0300

    added nginx build

---
+++ usr/src/.BUILD/nginx.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Dependencies which must exist prior to current build. If not found, will try to install
+DEPENDENCIES=(openssl pcre);
+
+# build data
+BUILD="../${PWD##*/}";
+VERSION="1.7.12";
+APP_NAME="nginx";
+
+# destination build info
+LOCAL="/opt/local";
+BIN_DIR="${LOCAL}/sbin";
+ETC_DIR="${LOCAL}/etc";
+DESTINATION="${BIN_DIR}/${APP_NAME}-${VERSION}";
+
+source ./helpers/.dependency_install.sh; ##checks all @DEPENDENCIES in tact
+source ./helpers/.pre_build_unpack.sh; ##unpack tar and enters the app dir
+
+./configure \
+--prefix=${DESTINATION} \
+--conf-path=${ETC_DIR}/${APP_NAME}/nginx.conf \
+--http-log-path=/var/log/${APP_NAME}/access.log \
+--error-log-path=/var/log/${APP_NAME}/error.log \
+--lock-path=/run/lock/subsys/${APP_NAME}.lock \
+--pid-path=/run/${APP_NAME}.pid \
+--user=${APP_NAME} \
+--group=${APP_NAME} \
+--with-debug \
+--with-file-aio \
+--with-google_perftools_module \
+--with-http_addition_module \
+--with-http_dav_module \
+--with-http_degradation_module \
+--with-http_flv_module \
+--with-http_geoip_module \
+--with-http_gunzip_module \
+--with-http_gzip_static_module \
+--with-http_image_filter_module \
+--with-http_mp4_module \
+--with-http_perl_module \
+--with-http_random_index_module \
+--with-http_realip_module \
+--with-http_secure_link_module \
+--with-http_spdy_module \
+--with-http_ssl_module \
+--with-http_stub_status_module \
+--with-http_sub_module \
+--with-http_xslt_module \
+--with-ipv6 \
+--with-mail \
+--with-mail_ssl_module \
+--with-openssl=${BIN_DIR}/openssl \
+--with-pcre=${BIN_DIR}/pcre;
+
+make;
+make install;
+
+[ -a "${BUILD}/post_build/$0" ] && cd ${BUILD}/post_build; $0 ${BIN_DIR} ${APP_NAME} ${VERSION};
---
+++ usr/src/.BUILD/post_build/nginx.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+source ../helpers/.post_validate_input.sh;
+source ./.shared.sh;
+
+APP_DIR="${BIN_DIR}/${APP_NAME}-${VERSION}";
+ETC_DIR="/opt/local/etc";
+USER="$4";
+[ -z "${USER}" ] && usage "[error] User was not set. Halt. As apache setup requires a user.";
+
+## general init.d settings
+#../helpers/bin/ln.sh ${BIN_DIR}/${APP_NAME}/bin/nginx ${ETC_DIR}/init.d/${APP_NAME};
+#../helpers/post_etc_ln.sh "${ETC_DIR}" "init.d" "${APP_NAME}";
+
+## system stop/start on boot
+#update-rc.d ${APP_NAME} defaults
+
+## I like getting there faster..
+#../helpers/bin/ln.sh ${APP_DIR}/htdocs /var/www;
+
+## profile.d
+#../helpers/post_etc_ln.sh "${ETC_DIR}" "profile.d" "${APP_NAME}.sh";
+
+## logs
+../helpers/bin/ln.sh ${APP_DIR}/logs /var/log/${USER};
+
+## logrotate.d
+#../helpers/post_etc_ln.sh "${ETC_DIR}" "logrotate.d" "${APP_NAME}";
+
+## create vhost + php-fpm settings for first hold
+#../helpers/apache_useradd.sh ${USER} localhost 000 ${BIN_DIR}/${APP_NAME}/htdocs;
+
+## create and own logs for vhosts
+[ ! -d "/var/log/${USER}" ] && mkdir /var/log/${USER} && chown ${USER}:${USER} /var/log/${USER};
+
+## security modes and owner changes from root
+#chown -R ${USER}:${USER} ${BIN_DIR}/${APP_NAME};
+#chown -R ${USER}:${USER} ${APP_DIR};
+#chmod -R go-rwx ${APP_DIR};
+#chmod -R a-w ${APP_DIR};
+#chmod o+x ${APP_DIR} ${APP_DIR}/htdocs ${APP_DIR}/cgi-bin;
+#chmod -R o+r ${APP_DIR}/htdocs;
+#chmod -R u+w ${APP_DIR}/logs;
+
+## restart service
+#/etc/init.d/${APP_NAME} restart
