02a298feb966ff5b70b2cbc781681c35998313d9
Oleg <github.com@landroverz.com>
2015-04-24 10:18:01 +0300

    user added

--- usr/src/.BUILD/post_build/squid.sh
+++ usr/src/.BUILD/post_build/squid.sh
@@ -2,3 +2,11 @@
 
 source ../helpers/.post_validate_input.sh;
 source ./.shared.sh;
+
+APP_DIR="${BIN_DIR}/${APP_NAME}-${VERSION}";
+ETC_DIR="/opt/local/etc";
+USER="$4";
+[ -z "${USER}" ] && usage "[error] User was not set. Halt. As squid setup requires a user.";
+
+## create and own logs
+[ ! -d "/var/log/${APP_NAME}" ] && mkdir /var/log/${APP_NAME} && chown ${USER}:${USER} /var/log/${APP_NAME};
--- usr/src/.BUILD/squid.sh
+++ usr/src/.BUILD/squid.sh
@@ -13,15 +13,19 @@ LOCAL="/opt/local";
 BIN_DIR="${LOCAL}/sbin";
 ETC_DIR="${LOCAL}/etc";
 DESTINATION="${BIN_DIR}/${APP_NAME}-${VERSION}";
+USER="${APP_NAME}";
 
 source ./helpers/.dependency_install.sh; ##checks all @DEPENDENCIES in tact
 source ./helpers/.pre_build_unpack.sh; ##unpack tar and enters the app dir
+
+## Create user for squid
+[ -z "$(getent passwd ${USER})" ] && echo "[info] User ${USER} not found, creating.." && useradd -M -s /bin/false -d /dev/null ${USER};
 
 ./configure \
 --prefix=${DESTINATION} \
 --sysconfdir=${ETC_DIR}/${APP_NAME} \
 --with-openssl=${BIN_DIR}/openssl \
---with-default-user=${APP_NAME} \
+--with-default-user=${USER} \
 --with-swapdir=/dev/null \
 --with-pidfile=/var/run/${APP_NAME}.pid \
 --with-logdir=/var/log/${APP_NAME};
@@ -29,4 +33,4 @@ source ./helpers/.pre_build_unpack.sh; ##unpack tar and enters the app dir
 make;
 make install;
 
-[ -a "${BUILD}/post_build/$0" ] && cd ${BUILD}/post_build; $0 ${BIN_DIR} ${APP_NAME} ${VERSION};
+[ -a "${BUILD}/post_build/$0" ] && cd ${BUILD}/post_build; $0 ${BIN_DIR} ${APP_NAME} ${VERSION} ${USER};
