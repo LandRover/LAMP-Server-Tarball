8919417d9729d8ea164f95c002314c94997fd543
Oleg <github.com@landroverz.com>
2015-04-24 08:49:41 +0300

    squid added

---
+++ usr/src/.BUILD/squid.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# build data
+BUILD="../${PWD##*/}";
+VERSION="3.5.3";
+APP_NAME="squid";
+
+# destination build info
+LOCAL="/opt/local";
+BIN_DIR="${LOCAL}/sbin";
+ETC_DIR="${LOCAL}/etc";
+DESTINATION="${BIN_DIR}/${APP_NAME}-${VERSION}";
+
+source ./helpers/.dependency_install.sh; ##checks all @DEPENDENCIES in tact
+source ./helpers/.pre_build_unpack.sh; ##unpack tar and enters the app dir
+
+./configure \
+--prefix=${DESTINATION};
+
+make;
+make install;
+
+[ -a "${BUILD}/post_build/$0" ] && cd ${BUILD}/post_build; $0 ${BIN_DIR} ${APP_NAME} ${VERSION};
