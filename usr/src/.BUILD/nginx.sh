f58c81d20a81fa6484f49eaa19039f0a69f91838
Oleg <github.com@landroverz.com>
2015-04-26 19:21:11 +0300

    lib paths added

--- usr/src/.BUILD/nginx.sh
+++ usr/src/.BUILD/nginx.sh
@@ -25,6 +25,11 @@ source ./helpers/.pre_build_unpack.sh; ##unpack tar and enters the app dir
 --error-log-path=/var/log/${APP_NAME}/error.log \
 --lock-path=/var/run/lock/subsys/${APP_NAME}.lock \
 --pid-path=/var/run/${APP_NAME}.pid \
+--http-client-body-temp-path=${BIN_DIR}/${APP_NAME}/lib/body \
+--http-fastcgi-temp-path=${BIN_DIR}/${APP_NAME}/lib/fastcgi \
+--http-proxy-temp-path=${BIN_DIR}/${APP_NAME}/lib/proxy \
+--http-scgi-temp-path=${BIN_DIR}/${APP_NAME}/lib/scgi \
+--http-uwsgi-temp-path=${BIN_DIR}/${APP_NAME}/lib/uwsgi \
 --user=${APP_NAME} \
 --group=${APP_NAME} \
 --with-debug \
@@ -51,8 +56,8 @@ source ./helpers/.pre_build_unpack.sh; ##unpack tar and enters the app dir
 --with-ipv6 \
 --with-mail \
 --with-mail_ssl_module \
---with-cc-opt='-g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -I/opt/local/sbin/libxml2/include/libxml2 -I/opt/local/sbin/libxslt/include -I/opt/local/sbin/libgd/include -I/opt/local/sbin/GeoIP/include -I/opt/local/sbin/gperftools/include -I/opt/local/sbin/pcre/include -I/opt/local/sbin/openssl/include -I/opt/local/sbin/zlib/include' \
---with-ld-opt='-Wl,-z,relro -Wl,--as-needed -L/opt/local/sbin/libxml2/lib -L/opt/local/sbin/libxslt/lib -L/opt/local/sbin/libgd/lib -L/opt/local/sbin/GeoIP/lib -L/opt/local/sbin/gperftools/lib -L/opt/local/sbin/pcre/lib -L/opt/local/sbin/openssl/lib -L/opt/local/sbin/zlib/lib';
+--with-cc-opt="-g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -I${BIN_DIR}/libxml2/include/libxml2 -I${BIN_DIR}/libxslt/include -I${BIN_DIR}/libgd/include -I${BIN_DIR}/GeoIP/include -I${BIN_DIR}/gperftools/include -I${BIN_DIR}/pcre/include -I${BIN_DIR}/openssl/include -I${BIN_DIR}/zlib/include" \
+--with-ld-opt="-Wl,-z,relro -Wl,--as-needed -L${BIN_DIR}/libxml2/lib -L${BIN_DIR}/libxslt/lib -L${BIN_DIR}/libgd/lib -L${BIN_DIR}/GeoIP/lib -L${BIN_DIR}/gperftools/lib -L${BIN_DIR}/pcre/lib -L${BIN_DIR}/openssl/lib -L${BIN_DIR}/zlib/lib";
 
 make;
 make install;
