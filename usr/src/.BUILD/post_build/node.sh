98e03b404f87046ee10af231c51444af9e3f54e4
OG <github.com@landroverz.com>
2018-06-21 07:04:14 +0300

    Update node.sh

--- usr/src/.BUILD/post_build/node.sh
+++ usr/src/.BUILD/post_build/node.sh
@@ -4,4 +4,4 @@ source ../helpers/.post_validate_input.sh;
 source ./.shared.sh;
 
 ## profile.d
-../helpers/post_etc_ln.sh "${ETC_DIR}" "profile.d" "${APP_NAME}.sh";
+../helpers/post_etc_ln.sh "/opt/local/etc" "profile.d" "${APP_NAME}.sh";
