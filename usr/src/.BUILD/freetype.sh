233f4d80f685f8f16142d2dc9d01ad28afa04538
OG <github.com@landroverz.com>
2018-06-23 08:27:43 +0300

    Update freetype.sh

--- usr/src/.BUILD/freetype.sh
+++ usr/src/.BUILD/freetype.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Dependencies which must exist prior to current build. If not found, will try to install
+DEPENDENCIES=(libpng);
+
 # build data
 VERSION="2.7.1";
 DIST_URL="http://download.savannah.gnu.org/releases/freetype/freetype-${VERSION}.tar.gz";
