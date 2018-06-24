2b843e904a04744d1b2aad16230b08571762ead5
OG <github.com@landroverz.com>
2018-06-24 12:40:31 +0300

    Create python_multi_version.sh

---
+++ usr/src/.BUILD/helpers/python_multi_version.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1;
+update-alternatives --install /usr/bin/python python /usr/bin/python3.4 2;
+update-alternatives --config python;
