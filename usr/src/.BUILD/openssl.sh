48285bd8184ea583a76ee80d45de63ce2051801c
OG <git@landroverz.com>
2020-04-12 12:12:30 +0300

    openssl version bump to 1.1.1f

--- usr/src/.BUILD/openssl.sh
+++ usr/src/.BUILD/openssl.sh
@@ -8,7 +8,7 @@ DEPENDENCIES=(zlib);
 #http://curl.haxx.se/ca/cacert.pem --> /opt/local/etc/openssl/certs/cacert.pem
 
 # build data
-VERSION="1.1.1";
+VERSION="1.1.1f";
 DIST_URL="https://www.openssl.org/source/openssl-${VERSION}.tar.gz";
 APP_NAME="openssl";
 
