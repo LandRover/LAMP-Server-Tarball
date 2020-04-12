ebf185585b18092722be86dc4d738423a9bc5c78
OG <git@landroverz.com>
2020-04-12 12:16:07 +0300

    Update exim.sh

--- usr/src/.BUILD/exim.sh
+++ usr/src/.BUILD/exim.sh
@@ -6,7 +6,7 @@ DEPENDENCIES=(pcre gdbm openssl);
 apt-get -y install libperl-dev libxaw7-dev libxt-dev;
 
 # build data
-VERSION="4.91";
+VERSION="4.93";
 DIST_URL="https://ftp.exim.org/pub/exim/exim4/exim-${VERSION}.tar.gz";
 APP_NAME="exim";
 USER="exim";
@@ -15,6 +15,8 @@ source ./helpers/build_pre/.pre-start.sh;
 
 ## Create user for exim
 [ -z "$(getent passwd ${USER})" ] && echo "[info] User ${USER} not found, creating.." && useradd -M -s /bin/false -d /dev/null ${USER};
+
+export USE_OPENSSL=yes
 
 ## Settings for build
 ${BUILD}/helpers/bin/ln.sh ${BUILD}/helpers/templates/${APP_NAME}/Makefile /usr/src/${APP_NAME}/Local/Makefile;
