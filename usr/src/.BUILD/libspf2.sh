64c4ef8f4193cbf730efd13176fc461e5eff6995
OG <git@landroverz.com>
2020-04-16 20:27:42 +0300

    libspf2 apt-get instead of compile

--- usr/src/.BUILD/libspf2.sh
+++ usr/src/.BUILD/libspf2.sh
@@ -5,23 +5,25 @@ VERSION="1.2.10";
 DIST_URL="https://www.libspf2.org/spf/libspf2-${VERSION}.tar.gz";
 APP_NAME="libspf2";
 
-source ./helpers/build_pre/.pre-start.sh;
+COMPILE_FROM_SOURCE=false;
 
-#apt-get -y install spf-tools-perl;
+if [ "$COMPILE_FROM_SOURCE" = true ] ; then
+    source ./helpers/build_pre/.pre-start.sh;
 
-## pending pull request fix: https://github.com/shevek/libspf2/pull/5/files
-## won't compile without.
-## sed 's/SPF_debugx( __FILE__, __LINE__, format, __VA_ARGS__ )/SPF_debugx( __FILE__, __LINE__, format, ##__VA_ARGS__ )/g' /usr/src/libspf2/src/include/spf_log.h
+    ## pending pull request fix: https://github.com/shevek/libspf2/pull/5/files
+    ## won't compile without.
+    ## sed 's/SPF_debugx( __FILE__, __LINE__, format, __VA_ARGS__ )/SPF_debugx( __FILE__, __LINE__, format, ##__VA_ARGS__ )/g' /usr/src/libspf2/src/include/spf_log.h
+    ## --host/--build set to 'x86_64-linux-gnu'
 
-./configure \
---prefix=${DESTINATION} \
---host=x86_64-linux-gnu \
---build=x86_64-linux-gnu;
+    ./configure \
+    --prefix=${DESTINATION};
 
-make;
-make check;
-make install;
-#make maintainer-clean
+    make;
+    make check;
+    make install;
+    #make maintainer-clean;
 
-
-cd ${BUILD}/helpers/build_post && /bin/bash ./.post-start.sh $0 ${BIN_DIR} ${ETC_DIR} ${APP_NAME} ${VERSION};
+    cd ${BUILD}/helpers/build_post && /bin/bash ./.post-start.sh $0 ${BIN_DIR} ${ETC_DIR} ${APP_NAME} ${VERSION};
+else
+    apt-get -y install spf-tools-perl;
+fi
