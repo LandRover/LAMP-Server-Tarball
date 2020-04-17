ec0681c0cd263435a7ba0d9f2874936dc7ed0e2e
OG <git@landroverz.com>
2020-04-17 10:48:58 +0300

    typo

--- usr/src/.BUILD/helpers/build_post/apps/openssl.sh
+++ usr/src/.BUILD/helpers/build_post/apps/openssl.sh
@@ -3,4 +3,4 @@
 CACERT_PATH="http://curl.haxx.se/ca/cacert.pem";
 
 echo "[info] Downloading CACERT from ${CACERT_PATH}";
-wget ${CACERT_PATH} -O ${ETC_DIR}/{$APP_NAME}/certs/cacert.pem
+wget ${CACERT_PATH} -O ${ETC_DIR}/${APP_NAME}/certs/cacert.pem
