b67baadb7767ece5dcc41166224cbfa7258536ce
LandRover <git@landroverz.com>
2020-04-20 14:13:10 +0000

    dovecot semi final configs and build scripts

--- usr/src/.BUILD/dovecot.sh
+++ usr/src/.BUILD/dovecot.sh
@@ -7,15 +7,22 @@ DEPENDENCIES=(openssl);
 VERSION="2.3.10";
 DIST_URL="https://dovecot.org/releases/2.3/dovecot-${VERSION}.tar.gz";
 APP_NAME="dovecot";
+USER="${APP_NAME}";
 
 source ./helpers/build_pre/.pre-start.sh;
+
+## Create user for exim
+[ -z "$(getent passwd ${USER})" ] && echo "[info] User ${USER} not found, creating.." && useradd -M -s /bin/false -d /dev/null ${USER};
+
 
 export CPPFLAGS="-I${BIN_DIR}/openssl/include";
 export LDFLAGS="-L${BIN_DIR}/openssl/lib";
 
 ./configure \
---prefix=${DESTINATION}\
---sysconfdir=${ETC_DIR}/${APP_NAME};
+--prefix=${DESTINATION} \
+--sysconfdir=${ETC_DIR} \
+--disable-largefile \
+--with-zlib;
 
 make;
 make install;
