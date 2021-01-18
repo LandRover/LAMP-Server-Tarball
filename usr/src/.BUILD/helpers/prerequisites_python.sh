786a7a37d5e909cd0b40bf6a1a7d2b96ebda8b31
OG <github.com@landroverz.com>
2021-01-18 19:23:55 +0200

    Update and rename prerequisites_python.sh to prerequisites_python3.sh

--- usr/src/.BUILD/helpers/prerequisites_python.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt-get -y install \
-python-pip \
-python-setuptools \
-python-dev \
-build-essential;
-
-pip install --upgrade virtualenv;
---
+++ usr/src/.BUILD/helpers/prerequisites_python3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-get -y install \
+python3-pip \
+python3-setuptools \
+python3-dev \
+build-essential;
+
+pip3 install --upgrade virtualenv;
+
+update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1;
+update-alternatives --install /usr/bin/python python /usr/bin/python3.7 2;
+update-alternatives --config python;
