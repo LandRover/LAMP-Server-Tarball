89978890a8bc0980269c8176e87327a150e050db
OG <github.com@landroverz.com>
2021-11-20 20:50:16 +0200

    libgd 2.3.3

--- usr/src/.BUILD/libgd.sh
+++ usr/src/.BUILD/libgd.sh
@@ -4,7 +4,7 @@
 DEPENDENCIES=(zlib jpeg libpng libwebp freetype fontconfig);
 
 # build data
-VERSION="2.3.0";
+VERSION="2.3.3";
 DIST_URL="https://github.com/libgd/libgd/releases/download/gd-${VERSION}/libgd-${VERSION}.tar.gz";
 APP_NAME="libgd";
 
