3facacf8081d3f2db2e22074f50ac5de4706411a
OG <github.com@landroverz.com>
2021-11-20 20:17:33 +0200

    mysql 8.0.27

--- usr/src/.BUILD/mysql.sh
+++ usr/src/.BUILD/mysql.sh
@@ -6,7 +6,7 @@ DEPENDENCIES=(openssl curl);
 apt-get -y install patchelf;
 
 # build data
-VERSION="8.0.20";
+VERSION="8.0.27";
 DIST_URL="https://cdn.mysql.com//Downloads/MySQL-8.0/mysql-${VERSION}.tar.gz";
 APP_NAME="mysql";
 USER="mysql";
