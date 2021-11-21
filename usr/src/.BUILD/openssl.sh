c239b8b9586289745d8b79aca04e279c0868e294
OG <github.com@landroverz.com>
2021-11-21 18:00:19 +0200

    openssl 3.0.0

--- usr/src/.BUILD/openssl.sh
+++ usr/src/.BUILD/openssl.sh
@@ -8,7 +8,7 @@ DEPENDENCIES=(zlib);
 #http://curl.haxx.se/ca/cacert.pem --> /opt/local/etc/openssl/certs/cacert.pem
 
 # build data
-VERSION="1.1.1l";
+VERSION="3.0.0";
 DIST_URL="https://www.openssl.org/source/openssl-${VERSION}.tar.gz";
 APP_NAME="openssl";
 
