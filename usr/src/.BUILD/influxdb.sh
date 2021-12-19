fa88dca77c03d3303d55c8b925646a4ad52c98fe
OG <github.com@landroverz.com>
2021-12-19 18:15:24 +0200

    influx 2.1.1

---
+++ usr/src/.BUILD/influxdb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# build data
+VERSION="2.1.1";
+DIST_URL="https://dl.influxdata.com/influxdb/releases/influxdb2-${VERSION}-linux-amd64.tar.gz";
+APP_NAME="cmake";
+
+source ./helpers/build_pre/.pre-start.sh;
+
+echo "Done. Making ${APP_NAME}-${VERSION}...";
+echo "Trying to make ${APP_NAME}...";
+make || die 0 "[${APP_NAME}] Make failed";
+
+make install || die 0 "[${APP_NAME}] Make install failed";
+echo "Done ${APP_NAME}.";
+
+cd ${BUILD}/helpers/build_post && /bin/bash ./.post-start.sh $0 ${BIN_DIR} ${ETC_DIR} ${APP_NAME} ${VERSION};
