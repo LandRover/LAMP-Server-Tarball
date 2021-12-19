fb06338152e891e4620b1e599dd2ba360bf808c0
OG <github.com@landroverz.com>
2021-12-19 21:21:23 +0200

    Update .pre-start.sh

--- usr/src/.BUILD/helpers/build_pre/.pre-start.sh
+++ usr/src/.BUILD/helpers/build_pre/.pre-start.sh
@@ -1,5 +1,6 @@
 source ./helpers/build_pre/.pre-global-vars.sh;
-source ./helpers/build_pre/.pre-dependency-install.sh; ##checks all @DEPENDENCIES in tact
-source ./helpers/build_pre/.pre-build-unpack.sh; ##unpack tar and enters the app dir
+source ./helpers/build_pre/.pre-dependency-install.sh;     # Checks all @DEPENDENCIES in tact
+source ./helpers/build_pre/.pre-build-unpack.sh;           # Unpack tar and enters the app dir
+source ./helpers/build_pre/.alias.sh;                      # Handle alias creation
 
 echo "Configuring ${APP_NAME}-${VERSION}...";
