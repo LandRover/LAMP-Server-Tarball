993795b6c53df23adfa92a90f87ca76aaed969f6
OG <github.com@landroverz.com>
2021-12-20 07:04:30 +0200

    Update influxdb2-client.sh

--- usr/src/.BUILD/influxdb2-client.sh
+++ usr/src/.BUILD/influxdb2-client.sh
@@ -9,7 +9,7 @@ source ./helpers/build_pre/.pre-start.sh;
 
 [ -d "${BIN_DIR}/${APP_NAME}-${VERSION}/bin" ] && echo "[INFO] Detected previous install of ${APP_NAME}, Version: ${VERSION}. Removing..." && rm -rf ${BIN_DIR}/${APP_NAME}-${VERSION};
 mkdir -p ${BIN_DIR}/${APP_NAME}-${VERSION}/bin;
-mv ../${APP_NAME}-${VERSION}/influx ${BIN_DIR}/${APP_NAME}-${VERSION}/bin/;
+cp ../${APP_NAME}-${VERSION}/influx ${BIN_DIR}/${APP_NAME}-${VERSION}/bin/;
 
 echo "Done ${APP_NAME}.";
 
