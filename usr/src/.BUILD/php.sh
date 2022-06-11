135d9c01331202efba338dc4691db46979860f48
OG <github.com@landroverz.com>
2022-06-11 17:14:54 +0300

    latest 7.4, 7.4.30

--- usr/src/.BUILD/php.sh
+++ usr/src/.BUILD/php.sh
@@ -4,7 +4,7 @@
 DEPENDENCIES=(jpeg libpng libwebp libgd freetype libxml2 pcre openssl zlib mysql libzip curl);
 
 # build data
-VERSION="7.4.5";
+VERSION="7.4.30";
 DIST_URL="http://cz1.php.net/distributions/php-${VERSION}.tar.gz";
 APP_NAME="php";
 
