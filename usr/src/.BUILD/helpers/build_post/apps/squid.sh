4f15a3e81383555e3af64dfcf20e1da2437bd34f
OG <github.com@landroverz.com>
2022-10-14 13:17:00 +0300

    Update squid.sh

--- usr/src/.BUILD/helpers/build_post/apps/squid.sh
+++ usr/src/.BUILD/helpers/build_post/apps/squid.sh
@@ -5,3 +5,10 @@ USER="${PARAM1}";
 
 ## create and own logs
 [ ! -d "/var/log/${APP_NAME}" ] && mkdir /var/log/${APP_NAME} && chown ${USER}:${USER} /var/log/${APP_NAME};
+
+## create and own logs
+[ ! -d "/etc/${APP_NAME}/certs" ] && mkdir /etc/${APP_NAME}/certs && chown ${USER}:${USER} /etc/${APP_NAME}/certs
+
+openssl req -new -newkey rsa:2048 -sha256 -days 365 -nodes -x509 -extensions v3_ca -keyout /etc/${APP_NAME}/certs/squid-ca-key.pem -out /etc/${APP_NAME}/certs/squid-ca-cert.pem
+cat /etc/${APP_NAME}/certs/squid-ca-cert.pem /etc/${APP_NAME}/certs/squid-ca-key.pem >> /etc/${APP_NAME}/certs/squid-ca-cert-key.pem
+chown ${USER}:${USER} -R /etc/${APP_NAME}/certs
