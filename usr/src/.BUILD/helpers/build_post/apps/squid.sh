2a8762f925dfb9198520887a5f95ec0cda3ccf31
OG <github.com@landroverz.com>
2022-10-28 12:46:21 +0300

    Update squid.sh

--- usr/src/.BUILD/helpers/build_post/apps/squid.sh
+++ usr/src/.BUILD/helpers/build_post/apps/squid.sh
@@ -12,3 +12,6 @@ USER="${PARAM1}";
 openssl req -new -newkey rsa:2048 -sha256 -days 365 -nodes -x509 -extensions v3_ca -keyout /etc/${APP_NAME}/certs/squid-ca-key.pem -out /etc/${APP_NAME}/certs/squid-ca-cert.pem
 cat /etc/${APP_NAME}/certs/squid-ca-cert.pem /etc/${APP_NAME}/certs/squid-ca-key.pem >> /etc/${APP_NAME}/certs/squid-ca-cert-key.pem
 chown ${USER}:${USER} -R /etc/${APP_NAME}/certs
+
+# auto start
+update-rc.d ${APP_NAME} defaults
