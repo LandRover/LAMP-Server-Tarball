51ff10dfb04b496d653d78488a99ea34a9e50137
OG <github.com@landroverz.com>
2022-10-28 18:16:10 +0300

    1.0.1

--- usr/src/.BUILD/fail2ban.sh
+++ usr/src/.BUILD/fail2ban.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ## libs required
-apt-get -y install python3-setuptools;
+apt-get -y install python3-setuptools 2to3;
 
 # build data
-VERSION="0.11.2";
+VERSION="1.0.1";
 DIST_URL="https://github.com/fail2ban/fail2ban/archive/${VERSION}.tar.gz";
 APP_NAME="fail2ban";
 USER="${APP_NAME}";
@@ -14,6 +14,8 @@ source ./helpers/build_pre/.pre-start.sh;
 [ -z "$(getent passwd ${USER})" ] && echo "[info] User ${USER} not found, creating.." && useradd --system -s /bin/false -d /dev/null --groups adm ${USER}
 
 echo "Trying to setup ${APP_NAME}...";
+
+./fail2ban-2to3;
 
 python3 setup.py install \
 --home=${DESTINATION} \
