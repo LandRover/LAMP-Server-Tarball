b0d155035644c2de9c8a737ca1bfd16919f629a0
OG <github.com@landroverz.com>
2022-10-30 13:24:32 +0200

    Update Anaconda3.sh

--- usr/src/.BUILD/Anaconda3.sh
+++ usr/src/.BUILD/Anaconda3.sh
@@ -10,8 +10,18 @@ APP_NAME="Anaconda3";
 
 source ./helpers/build_pre/.pre-start.sh;
 
+# start installer
 bash ./${APP_NAME}-${VERSION}.tgz
 
+# Python version
+python -v
+
+# update Anaconda
+conda update --all --yes
+
 echo "Done ${APP_NAME}.";
+
+# start:
+jupyter notebook
 
 cd ${BUILD}/helpers/build_post && /bin/bash ./.post-start.sh $0 ${BIN_DIR} ${ETC_DIR} ${APP_NAME} ${VERSION};
