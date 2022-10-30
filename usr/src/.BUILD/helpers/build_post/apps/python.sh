119e279906cffe2550d7a661b9dae00a1e98d6c9
OG <github.com@landroverz.com>
2022-10-30 10:27:14 +0200

    python

--- usr/src/.BUILD/helpers/build_post/apps/python.sh
+++ usr/src/.BUILD/helpers/build_post/apps/python.sh
@@ -4,6 +4,6 @@
 ${BIN_DIR}/${APP_NAME}/bin/${APP_NAME}3 -c "import sysconfig; print(sysconfig.get_config_vars('CONFIG_ARGS'));"
 
 # set primary
-update-alternatives --install /usr/bin/python python ${BIN_DIR}/${APP_NAME}/bin/python3 1;
 update-alternatives --install /usr/bin/pip pip ${BIN_DIR}/${APP_NAME}/bin/pip3 1;
 update-alternatives --install /usr/bin/pip3 pip3 ${BIN_DIR}/${APP_NAME}/bin/pip3 1;
+update-alternatives --install /usr/bin/python python ${BIN_DIR}/${APP_NAME}/bin/python3 1;
