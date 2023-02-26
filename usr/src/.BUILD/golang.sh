e33e1539c04c8f8251bc81218ce7efafac81d9d0
OG <github.com@landroverz.com>
2023-02-26 08:30:15 +0200

    1.20.1

--- usr/src/.BUILD/golang.sh
+++ usr/src/.BUILD/golang.sh
@@ -4,7 +4,7 @@
 DEPENDENCIES=(openssl libidn2 nghttp2 zlib);
 
 # build data
-VERSION="1.19.4";
+VERSION="1.20.1";
 DIST_URL="https://storage.googleapis.com/golang/go${VERSION}.linux-amd64.tar.gz";
 APP_NAME="go";
 
