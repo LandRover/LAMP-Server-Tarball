3139963c61a0534b9ad61a289337a57279da6d65
OG <git@landroverz.com>
2023-04-25 11:53:53 +0300

    8.2.5

--- usr/src/.BUILD/php.sh
+++ usr/src/.BUILD/php.sh
@@ -4,7 +4,7 @@
 DEPENDENCIES=(jpeg libpng libwebp libgd freetype libxml2 pcre openssl zlib mysql libzip curl);
 
 # build data
-VERSION="8.2.3";
+VERSION="8.2.5";
 DIST_URL="http://cz1.php.net/distributions/php-${VERSION}.tar.gz";
 APP_NAME="php";
 
