5c343b87321e38fa672e8386d8541b39a4798339
OG <git@landroverz.com>
2023-04-26 19:50:28 +0300

    5.2.1

--- usr/src/.BUILD/phpMyAdmin.sh
+++ usr/src/.BUILD/phpMyAdmin.sh
@@ -4,7 +4,7 @@
 DEPENDENCIES=(httpd php);
 
 # build data
-VERSION="5.2.0";
+VERSION="5.2.1";
 DIST_URL="https://files.phpmyadmin.net/phpMyAdmin/${VERSION}/phpMyAdmin-${VERSION}-english.tar.gz";
 APP_NAME="phpMyAdmin";
 USER="apache";
