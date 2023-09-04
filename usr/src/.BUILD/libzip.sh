c377590c1cbf56e65f34fe4f1e09b772193d36a4
OG <git@landroverz.com>
2023-09-04 12:46:36 +0300

    1.10.1

--- usr/src/.BUILD/libzip.sh
+++ usr/src/.BUILD/libzip.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Dependencies which must exist prior to current build. If not found, will try to install
+# Dependencies that must exist prior to the current build. If not found, will try to install
 DEPENDENCIES=(zlib);
 
 # build data
-VERSION="1.9.2";
+VERSION="1.10.1";
 DIST_URL="https://libzip.org/download/libzip-${VERSION}.tar.gz";
 APP_NAME="libzip";
 
