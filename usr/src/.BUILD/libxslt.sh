c67a6aaaa23870f39f6900cdb0e5c311b1d5e362
OG <git@landroverz.com>
2024-06-22 19:42:06 +0300

    1.1.41

--- usr/src/.BUILD/libxslt.sh
+++ usr/src/.BUILD/libxslt.sh
@@ -4,7 +4,7 @@
 DEPENDENCIES=(libxml2 libgcrypt);
 
 # build data
-VERSION="1.1.38";
+VERSION="1.1.41";
 DIST_URL="https://ftp.osuosl.org/pub/blfs/conglomeration/libxslt/libxslt-${VERSION}.tar.xz";
 APP_NAME="libxslt";
 
