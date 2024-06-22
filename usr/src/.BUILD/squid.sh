b95caf6c731e9181a9d9a88319bda6cc23d3e1ae
OG <git@landroverz.com>
2024-06-22 19:32:14 +0300

    6.10

--- usr/src/.BUILD/squid.sh
+++ usr/src/.BUILD/squid.sh
@@ -4,7 +4,7 @@
 DEPENDENCIES=(openssl gnutls libecap libxml2);
 
 # build data
-VERSION="6.3";
+VERSION="6.10";
 DIST_URL="http://www.squid-cache.org/Versions/v6/squid-${VERSION}.tar.gz";
 APP_NAME="squid";
 USER="${APP_NAME}";
