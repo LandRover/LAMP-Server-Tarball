e9c72885628450e934e1cb6e9d6dcce01bde0ae2
OG <git@landroverz.com>
2024-07-12 16:42:54 +0300

    python dep

--- usr/src/.BUILD/scons.sh
+++ usr/src/.BUILD/scons.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Dependencies which must exist prior to current build. If not found, will try to install
+DEPENDENCIES=(python);
+
 # build data
 VERSION="4.7.0";
 DIST_URL="https://netix.dl.sourceforge.net/project/scons/scons/${VERSION}/SCons-${VERSION}.tar.gz";
