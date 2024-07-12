7f3b0c531f4e11a292d0ca373ecc95b3c84e4655
OG <git@landroverz.com>
2024-07-12 18:45:58 +0300

    check_dependencies

--- usr/src/.BUILD/helpers/build_pre/.pre-dependency-install.sh
+++ usr/src/.BUILD/helpers/build_pre/.pre-dependency-install.sh
@@ -5,9 +5,23 @@
 # Unpacks source packages at /usr/src
 # file is loaded via SOURCE and must NOT BE executed directly.
 
-function deps_error {
-    echo $1;
-    echo "";
+check_dependencies() {
+  local any_dependency_missing=true
 
-    exit 0;
+  # Check each dependency
+  for DEP in "${DEPENDENCIES[@]}"; do
+    if [ ! -f "$BIN_DIR/$DEP" ]; then
+      echo "Error: Dependency '$DEP' not found in $BIN_DIR.";
+    else
+      echo "$DEP found in $BIN_DIR";
+      any_dependency_missing=false
+    fi
+  done
+
+  # Exit with status 1 if any dependency is missing
+  if [ "$any_dependency_missing" = true ]; then
+    exit 1
+  fi
 }
+
+check_dependencies
