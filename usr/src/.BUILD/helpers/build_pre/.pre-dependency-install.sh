0b6c4eb79c0e56cbba8151f725d630429cd7cd84
OG <git@landroverz.com>
2024-07-12 18:51:15 +0300

    removed func to echo properlly

--- usr/src/.BUILD/helpers/build_pre/.pre-dependency-install.sh
+++ usr/src/.BUILD/helpers/build_pre/.pre-dependency-install.sh
@@ -5,23 +5,19 @@
 # Unpacks source packages at /usr/src
 # file is loaded via SOURCE and must NOT BE executed directly.
 
-check_dependencies() {
-  local any_dependency_missing=true
+any_dependencies_found=true
 
-  # Check each dependency
-  for DEP in "${DEPENDENCIES[@]}"; do
-    if [ ! -f "$BIN_DIR/$DEP" ]; then
-      echo "Error: Dependency '$DEP' not found in $BIN_DIR.";
-    else
-      echo "$DEP found in $BIN_DIR";
-      any_dependency_missing=false
-    fi
-  done
-
-  # Exit with status 1 if any dependency is missing
-  if [ "$any_dependency_missing" = true ]; then
-    exit 1
+# Check each dependency
+for DEP in "${DEPENDENCIES[@]}"; do
+  if [ ! -f "${BIN_DIR}/${DEP}" ]; then
+    echo "Error: Dependency '${DEP}' not found in ${BIN_DIR}."
+    any_dependencies_found=false
+  else
+    echo "$DEP found in $BIN_DIR."
   fi
-}
+done
 
-check_dependencies
+# Exit with status 1 if any dependency is missing
+if [ "${any_dependencies_found}" = false ]; then
+  exit 1
+fi
