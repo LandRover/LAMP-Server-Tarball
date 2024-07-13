108bd9fd06c0360c5adf63e0736f419437331f5f
OG <git@landroverz.com>
2024-07-13 16:46:39 +0300

    Create swap_file_create.sh

---
+++ usr/src/.BUILD/helpers/swap_file_create.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# create swap file
+fallocate -l 1512M /large_swap_file
+chmod 600 /large_swap_file
+mkswap /large_swap_file
+
+ # add to /etc/fstb:
+ # /swapfile swap swap defaults 0 0
+ 
