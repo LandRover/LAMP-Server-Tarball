b4116ab71d32d71f04d0832e48b16ed13484486d
OG <git@landroverz.com>
2024-07-16 12:13:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,18 +6,18 @@ LOCAL_DIR="/LAMP-Server-Tarball";
 # Check if git is installed
 if ! command -v git &> /dev/null; then
     echo "[INFO] Git is not installed. Installing...";
-    sudo apt-get update;
-    sudo apt-get install -y git;
+    apt-get update;
+    apt-get install -y git;
 fi
 
 # Check if the repository directory exists
 if [ ! -d "${LOCAL_DIR}" ]; then
     # Clone the repository if it doesn't exist
     echo "[INFO] Cloning repository ${REPO_URL} to ${LOCAL_DIR}";
-    sudo git clone "${REPO_URL}" "${LOCAL_DIR}";
+    git clone "${REPO_URL}" "${LOCAL_DIR}";
 else
     # Pull updates if the repository already exists
     echo "[INFO] Repository already exists. Updating...";
     cd "${LOCAL_DIR}";
-    sudo git pull
+    git pull
 fi
