7438b5eb653d791a1eba94993d52938cd06d0af7
OG <git@landroverz.com>
2024-07-18 21:30:55 +0300

    added comment why these paths removed

--- usr/src/.BUILD/helpers/build_post/apps/openssh.sh
+++ usr/src/.BUILD/helpers/build_post/apps/openssh.sh
@@ -3,6 +3,7 @@
 ../bin/ln.sh "${ETC_DIR}/${APP_NAME}" "/etc/ssh";
 ../bin/ln.sh "${ETC_DIR}/init.d/${APP_NAME}" "/etc/init.d/ssh";
 
+# Cleanup original configs of ssh, causes issues to autostart if present.
 rm -rf /etc/systemd/system/ssh.service.d;
 rm -rf /lib/systemd/system/ssh.service;
 rm -rf /etc/systemd/system/sshd-keygen@.service.d;
