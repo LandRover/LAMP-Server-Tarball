6cf17d6256674da6df7d582e5b915faf24fef556
OG <git@landroverz.com>
2024-07-18 21:40:32 +0300

    wildcard ssh+sshd

--- usr/src/.BUILD/helpers/build_post/apps/openssh.sh
+++ usr/src/.BUILD/helpers/build_post/apps/openssh.sh
@@ -4,8 +4,7 @@
 ../bin/ln.sh "${ETC_DIR}/init.d/${APP_NAME}" "/etc/init.d/ssh";
 
 # Cleanup original configs of ssh, causes issues to autostart if present.
-rm -rf /lib/systemd/system/ssh.service;
-rm -rf /lib/systemd/system/ssh.service/sshd.service;
+rm -rf /lib/systemd/system/ssh*.service;
 rm -rf /etc/systemd/system/ssh.service.d;
 rm -rf /etc/systemd/system/sshd-keygen@.service.d;
 
