2f5fc8326cd9f634b4be2338388612724b79ec36
OG <git@landroverz.com>
2024-07-18 21:29:41 +0300

    remove old paths

--- usr/src/.BUILD/helpers/build_post/apps/openssh.sh
+++ usr/src/.BUILD/helpers/build_post/apps/openssh.sh
@@ -3,6 +3,10 @@
 ../bin/ln.sh "${ETC_DIR}/${APP_NAME}" "/etc/ssh";
 ../bin/ln.sh "${ETC_DIR}/init.d/${APP_NAME}" "/etc/init.d/ssh";
 
+rm -rf /etc/systemd/system/ssh.service.d;
+rm -rf /lib/systemd/system/ssh.service;
+rm -rf /etc/systemd/system/sshd-keygen@.service.d;
+
 # cleanup pre-installed app
 apt-get remove -y ${APP_NAME}-server;
 apt-get autoremove;
