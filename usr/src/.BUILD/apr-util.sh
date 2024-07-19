6cb270d2f95aad306138fee6d993fb44bfbb65d0
OG <git@landroverz.com>
2024-07-19 15:43:44 +0300

    expat dep added

--- usr/src/.BUILD/apr-util.sh
+++ usr/src/.BUILD/apr-util.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Dependencies which must exist prior to current build. If not found, will try to install
-DEPENDENCIES=(apr);
+DEPENDENCIES=(libexpat apr);
 
 # build data
 VERSION="1.6.3";
@@ -13,6 +13,7 @@ source ./helpers/build_pre/.pre-start.sh;
 ./configure \
 --prefix=${DESTINATION} \
 --with-apr=${BIN_DIR}/apr \
+--with-expat=${BIN_DIR}/libexpat \
 || die 0 "[${APP_NAME}] Configure failed";
 
 echo "Done. Making ${APP_NAME}-${VERSION}...";
@@ -20,6 +21,7 @@ echo "Trying to make ${APP_NAME}...";
 make || die 0 "[${APP_NAME}] Make failed";
 
 make install || die 0 "[${APP_NAME}] Make install failed";
+
 echo "Done ${APP_NAME}.";
 
 cd ${BUILD}/helpers/build_post && /bin/bash ./.post-start.sh $0 ${BIN_DIR} ${ETC_DIR} ${APP_NAME} ${VERSION};
