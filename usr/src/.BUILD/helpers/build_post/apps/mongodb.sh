a574c76c95c46591ea111f1c4355705116f01d63
OG <git@landroverz.com>
2024-07-19 14:52:35 +0300

    ${BIN_DIR}/openssl/bin/openssl

--- usr/src/.BUILD/helpers/build_post/apps/mongodb.sh
+++ usr/src/.BUILD/helpers/build_post/apps/mongodb.sh
@@ -7,7 +7,7 @@ PEM_CERT="${CERT_DIR}/${CERT_NAME}.pem";
 ## Veryify cert does NOT exist.. if not found will generate one
 if [[ ! -e "${PEM_CERT}" ]]; then
 	##generate crt and key
-	openssl req -new -x509 -days 3650 -nodes -out ${CERT_DIR}/${CERT_NAME}-cert.crt -keyout ${CERT_DIR}/${CERT_NAME}-cert.key;
+	${BIN_DIR}/openssl/bin/openssl req -new -x509 -days 3650 -nodes -out ${CERT_DIR}/${CERT_NAME}-cert.crt -keyout ${CERT_DIR}/${CERT_NAME}-cert.key;
 	
 	## combine key and cert into a single file
 	cat ${CERT_DIR}/${CERT_NAME}-cert.key ${CERT_DIR}/${CERT_NAME}-cert.crt > ${PEM_CERT};
