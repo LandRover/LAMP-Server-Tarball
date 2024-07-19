d7ad3fd22bf3d254a102c7ea61bc2ddeaa8eba2f
OG <git@landroverz.com>
2024-07-19 15:10:52 +0300

    spool

--- usr/src/.BUILD/helpers/build_post/apps/squid.sh
+++ usr/src/.BUILD/helpers/build_post/apps/squid.sh
@@ -3,14 +3,18 @@
 USER="${PARAM1}";
 CERT_PATH="${ETC_DIR}/${APP_NAME}/certs";
 LOG_PATH="/var/log/${APP_NAME}";
+SPOOL_PATH="/var/spool/squid/cache";
 
 [ -z "${USER}" ] && usage "[error] User was not set. Halt. As squid setup requires a user.";
 
-## create and own logs
+## Create LOG
 [ ! -d "${LOG_PATH}" ] && mkdir ${LOG_PATH} && chown ${USER}:${USER} ${LOG_PATH};
 
-## create and own certs
+## Create CERT
 [ ! -d "${CERT_PATH}" ] && mkdir -p ${CERT_PATH} && chown ${USER}:${USER} ${CERT_PATH};
+
+## Create SPOOL
+[ ! -d "${SPOOL_PATH}" ] && mkdir -p ${SPOOL_PATH} && chown ${USER}:${USER} ${SPOOL_PATH};
 
 ${BIN_DIR}/openssl/bin/openssl req -new -newkey rsa:2048 -sha256 -days 365 -nodes -x509 -extensions v3_ca -keyout ${CERT_PATH}/squid-ca-key.pem -out ${CERT_PATH}/squid-ca-cert.pem;
 
