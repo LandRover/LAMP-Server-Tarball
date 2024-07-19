341c07c7e6f41a0337747fec54c4d8b7f561a5f6
OG <git@landroverz.com>
2024-07-19 16:31:49 +0300

    proper path for zlib

--- usr/src/.BUILD/libzip.sh
+++ usr/src/.BUILD/libzip.sh
@@ -10,20 +10,17 @@ APP_NAME="libzip";
 
 source ./helpers/build_pre/.pre-start.sh;
 
-export LD_LIBRARY_PATH=${BIN_DIR}/zlib/lib;
-
 rm -rf build && mkdir -p build;
 cd build;
 
-cmake \
+cmake .. \
 -DCMAKE_INSTALL_PREFIX=${DESTINATION} \
 -DENABLE_OPENSSL=1 \
 -DENABLE_GNUTLS=OFF \
 -DENABLE_LZMA=OFF \
 -DENABLE_BZIP2=OFF \
--DZLIB_LIBRARY=${BIN_DIR}/zlib/lib \
+-DZLIB_LIBRARY=${BIN_DIR}/zlib/lib/libz.so \
 -DZLIB_INCLUDE_DIR=${BIN_DIR}/zlib/include \
-../ \
 || die 0 "[${APP_NAME}] Configure failed";
 
 echo "Done. Making ${APP_NAME}-${VERSION}...";
@@ -35,5 +32,7 @@ make test || die 0 "[${APP_NAME}] Make test failed";
 
 make install || die 0 "[${APP_NAME}] Make install failed";
 echo "Done ${APP_NAME}. It is recommended to reinstall libxslt after update of ${APP_NAME}.";
+
+cd ..;
 
 cd ${BUILD}/helpers/build_post && /bin/bash ./.post-start.sh $0 ${BIN_DIR} ${ETC_DIR} ${APP_NAME} ${VERSION};
