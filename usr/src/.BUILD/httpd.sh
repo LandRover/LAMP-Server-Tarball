1614a903de22649b62d89ce4787c7caf95e1267b
OG <git@landroverz.com>
2024-07-19 18:03:13 +0300

    2.4.62

--- usr/src/.BUILD/httpd.sh
+++ usr/src/.BUILD/httpd.sh
@@ -4,7 +4,7 @@
 DEPENDENCIES=(arp arp-util pcre openssl php);
 
 # build data
-VERSION="2.4.61";
+VERSION="2.4.62";
 DIST_URL="https://dlcdn.apache.org/httpd/httpd-${VERSION}.tar.gz";
 APP_NAME="httpd";
 USER="apache";
