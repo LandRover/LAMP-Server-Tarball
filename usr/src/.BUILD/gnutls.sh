7be7317f7701b1661e448882b284e2daa5d559eb
OG <git@landroverz.com>
2024-07-20 20:48:50 +0300

    3.8

--- usr/src/.BUILD/gnutls.sh
+++ usr/src/.BUILD/gnutls.sh
@@ -5,7 +5,7 @@ DEPENDENCIES=(libidn2 libtasn1 libunistring gmp nettle p11-kit zstd zlib);
 
 # build data
 VERSION="3.8.6";
-DIST_URL="https://www.gnupg.org/ftp/gcrypt/gnutls/v3.7/gnutls-${VERSION}.tar.xz";
+DIST_URL="https://www.gnupg.org/ftp/gcrypt/gnutls/v3.8/gnutls-${VERSION}.tar.xz";
 APP_NAME="gnutls";
 
 source ./helpers/build_pre/.pre-start.sh;
