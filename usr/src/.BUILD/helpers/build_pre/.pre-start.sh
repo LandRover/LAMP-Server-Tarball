e215b02e8b98b1d16f44cabe0d5f4407f586542a
OG <git@landroverz.com>
2024-07-21 10:20:40 +0300

    added bashrc inmc

--- usr/src/.BUILD/helpers/build_pre/.pre-start.sh
+++ usr/src/.BUILD/helpers/build_pre/.pre-start.sh
@@ -1,6 +1,8 @@
+source ~/.bashrc                                           # loads shared links pkg etc.
 source ./helpers/build_pre/.pre-global-vars.sh;
 source ./helpers/build_pre/.alias.sh;                      # Handle alias creation
 source ./helpers/build_pre/.pre-dependency-install.sh;     # Checks all @DEPENDENCIES in tact
 source ./helpers/build_pre/.pre-build-unpack.sh;           # Unpack tar and enters the app dir
+
 
 echo "Configuring ${APP_NAME}-${VERSION}...";
