c6a586009ceb6c56a76458044ab309cb4b44970d
OG <git@landroverz.com>
2024-07-21 22:03:40 +0300

    removed python_multi_version.sh, pyenv is by default installed

--- usr/src/.BUILD/helpers/python_multi_version.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1;
-update-alternatives --install /usr/bin/python python /usr/bin/python3.4 2;
-update-alternatives --config python;
