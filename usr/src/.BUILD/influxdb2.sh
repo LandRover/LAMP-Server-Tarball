34eb33350cdb001b09b2cff8677b2eca3762e9c4
OG <git@landroverz.com>
2024-07-21 16:35:05 +0300

    proper cp

--- usr/src/.BUILD/influxdb2.sh
+++ usr/src/.BUILD/influxdb2.sh
@@ -21,7 +21,7 @@ source ./helpers/build_pre/.pre-start.sh;
 ## INSTALL
 [ -d "${BIN_DIR}/${APP_NAME}-${VERSION}/bin" ] && echo "[INFO] Detected previous install of ${APP_NAME}, Version: ${VERSION}. Removing..." && rm -rf ${BIN_DIR}/${APP_NAME}-${VERSION};
 mkdir -p ${BIN_DIR}/${APP_NAME}-${VERSION}/bin;
-cp ../${APP_NAME}-${VERSION}/influxd ${BIN_DIR}/${APP_NAME}-${VERSION}/bin/;
+cp ../${APP_NAME}-${VERSION}/usr/bin/influxd ${BIN_DIR}/${APP_NAME}-${VERSION}/bin/;
 
 echo "Done ${APP_NAME}.";
 
