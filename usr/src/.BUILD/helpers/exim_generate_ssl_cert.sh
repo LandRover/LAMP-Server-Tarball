1d36e9e486f5a05adb3ef7b19b8c0ef2c1082076
OG <git@landroverz.com>
2024-07-25 10:05:09 +0300

    user

--- usr/src/.BUILD/helpers/exim_generate_ssl_cert.sh
+++ usr/src/.BUILD/helpers/exim_generate_ssl_cert.sh
@@ -5,7 +5,7 @@ mkdir -p /opt/local/etc/exim/ssl
 /opt/local/sbin/openssl/bin/openssl req -x509 -sha256 -days 9000 -nodes -newkey rsa:4096 -keyout /opt/local/etc/exim/ssl/exim.key -out /opt/local/etc/exim/ssl/exim.cert
 #/opt/local/sbin/openssl/bin/openssl req -x509 -newkey rsa:1024 -keyout /opt/local/etc/exim/ssl/exim.key -out /opt/local/etc/exim/ssl/exim.cert -days 9999 -nodes
 
-chown exim:exim /opt/local/etc/exim/ssl/exim.key
+chown mail:mail /opt/local/etc/exim/ssl/exim.key
 chmod 644 /opt/local/etc/exim/ssl/exim.key
 chmod 644 /opt/local/etc/exim/ssl/exim.cert
 
