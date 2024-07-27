f46b39a3b5a5bcf645d47614dddf9488f24c94f5
LandRover <git@landroverz.com>
2024-07-27 21:59:35 +0300

    calmav wip

--- usr/src/.BUILD/clamav.sh
+++ usr/src/.BUILD/clamav.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Dependencies that must exist prior to the current build. If not found, will try to install
+DEPENDENCIES=(cmake rust cargo);
+
 # build data
 VERSION="1.3.1";
 DIST_URL="https://github.com/Cisco-Talos/clamav/releases/download/clamav-${VERSION}/clamav-${VERSION}.tar.gz";
@@ -11,18 +14,27 @@ source ./helpers/build_pre/.pre-start.sh;
 ## Create user for clamav
 [ -z "$(getent passwd ${USER})" ] && echo "[info] User ${USER} not found, creating.." && useradd -M -s /bin/false -d /dev/null ${USER};
 
-./configure \
---prefix=${DESTINATION} \
---sysconfdir=${ETC_DIR} \
-CFLAGS="-I${BIN_DIR}/openssl/include" \
-LDFLAGS="-L${BIN_DIR}/openssl/lib64" \
---disable-largefile \
---with-pcre=${BIN_DIR}/pcre2 \
---with-zlib=${BIN_DIR}/zlib \
---with-xml=${BIN_DIR}/libxml2 \
---with-libcurl=${BIN_DIR}/curl \
---with-openssl=${BIN_DIR}/openssl \
+rm -rf build && mkdir build && cd build;
+
+cmake .. \
+  -DCMAKE_PREFIX_PATH="${DESTINATION}" \
+  -DAPP_CONFIG_DIRECTORY=${ETC_DIR}/clamav \
+  -DDATABASE_DIRECTORY=/var/lib/clamav \
+  -DENABLE_JSON_SHARED=OFF \
 || die 0 "[${APP_NAME}] Configure failed";
+
+#./configure \
+#--prefix=${DESTINATION} \
+#--sysconfdir=${ETC_DIR} \
+#CFLAGS="-I${BIN_DIR}/openssl/include" \
+#LDFLAGS="-L${BIN_DIR}/openssl/lib64" \
+#--disable-largefile \
+#--with-pcre=${BIN_DIR}/pcre2 \
+#--with-zlib=${BIN_DIR}/zlib \
+#--with-xml=${BIN_DIR}/libxml2 \
+#--with-libcurl=${BIN_DIR}/curl \
+#--with-openssl=${BIN_DIR}/openssl \
+#|| die 0 "[${APP_NAME}] Configure failed";
 
 echo "Done. Making ${APP_NAME}-${VERSION}...";
 echo "Trying to make ${APP_NAME}...";
@@ -30,5 +42,7 @@ make || die 0 "[${APP_NAME}] Make failed";
 
 make install || die 0 "[${APP_NAME}] Make install failed";
 echo "Done ${APP_NAME}.";
+
+cd ..;
 
 cd ${BUILD}/helpers/build_post && /bin/bash ./.post-start.sh $0 ${BIN_DIR} ${ETC_DIR} ${APP_NAME} ${VERSION};
